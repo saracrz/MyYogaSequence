49ca21b0480087f5d5168a878fecd9e4d9471076
Sara Cruz Betancor <sara.crz10@gmail.com>
2019-10-30 17:29:06 +0000

    fixed login with redirect

---
+++ front/scripts/api.js
@@ -0,0 +1,43 @@
+function API() {
+  if (!localStorage.getItem('token') && window.location.pathname !== '/login.html') { 
+    window.location.assign('login.html')
+  }
+
+  this.api_base = axios.create({
+    baseURL: "http://localhost:2222/api/",
+    timeout: 1000
+  });
+
+  this.login = function (userLogin) {
+    this.api_base
+      .post("auth/login", userLogin)
+      .then(response => {
+        if (response.data.error) {
+          alert('YOS TODO JAKER INTENTANDO ENTRAR')
+        } else {
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("name", response.data.username);
+          localStorage.setItem("email", response.data.email);
+          window.location.assign('index.html')
+        }
+      })
+      .catch(function (error) {
+        console.log(error.response);
+      });
+  };
+
+  this.signup = function (newUser) {
+    this.api_base
+      .post("auth/signup", newUser)
+      .then(function (response) {
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("name", response.data.name);
+        localStorage.setItem("email", response.data.email);
+      })
+      .catch(function (error) {
+        console.log(error.response);
+      });
+  }
+}
+const api = new API();
+
--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,13 +1,6 @@
-(function authenticated () {
-  if (localStorage.getItem('token')) {
-    console.log('user authenticated')
-  } else {
-    console.log('user not authenticated')
-  }
-})()
-
-const api = axios.create({
-  baseURL: "http://localhost:2222/api/",
-  timeout: 1000
-});
-
+document.getElementById('btn-login').addEventListener('click', (event) => {
+  api.login({
+    user_email:    document.getElementById("login_email").value,
+    user_password: document.getElementById("login_password").value
+  })
+})
